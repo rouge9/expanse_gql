c18cf3287c0799f4e9d0ad07d7b018882964764b
Robel Gedamu <99325915+rouge9@users.noreply.github.com>
2024-12-30 06:04:58 -0900

    Update cron.js

--- backend/cron.js
+++ backend/cron.js
@@ -41,7 +41,7 @@ const job = new cron.CronJob("*/14 * * * *", function () {
 //     .on("error", (e) => {
 //       console.error("Error while sending request", e);
 //     });
-// });
+});
 
 export default job;
 
